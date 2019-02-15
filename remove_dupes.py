b9e26fabc7c1ffcfb055130831f94f3b4919612b
bloorq@gmail.com <bloorq@gmail.com>
2019-02-15 00:26:08 +0000

    merge with quickhits.txt from SecLists; remove dupes

--- remove_dupes.py
+++ remove_dupes.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 #
 # script for removing duplicates in dictionary
+
 import sys
+
 dictionary = open(sys.argv[1], "r").readlines()
-print len(dictionary)
 res = set()
 for p in dictionary:
+    # remove trailing /:
+    if p[0] == '/':
+        p = p[1:]
+    # add uniqe entry to dictionary
     res.add(p.strip().lower())
     
 new_dict = list(res)
